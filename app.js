15e4d1ddb7894d2ee48cda518c7a479aa0803288
JacobUSC <jrr18@email.sc.edu>
2024-03-14 09:20:16 -0400

    stuff

---
+++ app.js
@@ -0,0 +1,7 @@
+var http = require("http");
+const port = process.env.PORT || 3001;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello World! 2");
+}).listen(port);
